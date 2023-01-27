8427c72931aa2a776489f307d344b383e8183ad9
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-01-27 22:14:38 +0600

    ok

--- arry.js
+++ arry.js
@@ -6,5 +6,13 @@ var name = ['nahid', 'jerin', 'romana', 'tanvir'];
 var numb1 = number[1];
 //console.log(numb1);
 
-number[3] = 100;
+//number[3] = 100;
+// console.log(number);
+
+number.push('nahid');
+// console.log(number);
+number.pop();
+
+
+var num3 = number.pop();
 console.log(number);
