c5d7f1cce2b2a35348ebd2aa2d4b8a5903ab8827
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-02-02 21:08:03 +0600

    ok

---
+++ even-odd.js
@@ -0,0 +1,12 @@
+function EvenOddChecker (num) {
+    let value = num % 2;
+    if(value == 1){
+        console.log('the number is Odd');
+    }
+    else{
+        console.log('the number is Even');
+    }
+
+}
+
+EvenOddChecker(7);
--- js-function-object.js
+++ js-function-object.js
@@ -47,15 +47,16 @@ for(var keys in shoppingCart){
 }
 
 
-var num = 2;
-switch(num){
-    case 1:
-        console.log('this is 1');
-        break;
-    case 2:
-        console.log('this is 2');
-        break;
-    case 3:
-        console.log('this is 3');
-        break;
-}
+// var num = 2;
+// switch(num){
+//     case 1:
+//         console.log('this is 1');
+//         break;
+//     case 2:
+//         console.log('this is 2');
+//         break;
+//     case 3:
+//         console.log('this is 3');
+//         break;
+// }
+
